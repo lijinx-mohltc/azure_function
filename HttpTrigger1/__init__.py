76701b75be8fdeeb541fe945465db3594eecd71d
lijinx-mohltc <jinxing.li@ontario.ca>
2020-03-31 12:32:48 -0400

    add validation

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -1,22 +1,24 @@
 import logging
 
 import azure.functions as func
-
+from openpyxl import Workbook, load_workbook
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
+    file = req.body('file')
+    if not file:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            file = req_body.get('file')
 
-    if name:
-        return func.HttpResponse(f"Hello {name}!")
+    if file:   
+        # wb = load_workbook(file)  
+        return func.HttpResponse(file)
+        # return func.HttpResponse(wb.get_sheet_names())
     else:
         return func.HttpResponse(
              "Please pass a name on the query string or in the request body",
