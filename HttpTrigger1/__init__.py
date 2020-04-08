72df169cef6899a64ffcc731452d1706d54f6e69
Jason Li <jasonli@Jasons-MacBook-Pro.local>
2020-04-08 18:27:11 -0400

    add mult excel sheet

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -10,11 +10,11 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     file = req.get_json()
-    decrypted=base64.b64decode(file['$content'])
+    decrypted=base64.b64decode(file['body']['$content'])
     toread = io.BytesIO()
     toread.write(decrypted)
     # toread.seek(0)
-    df = pd.read_excel(toread, usecols="C:K",skiprows=7)
+    df = pd.read_excel(toread, sheet_name='Fin_CMHP1', usecols="C:K",skiprows=7)
     df.rename(columns={"LHIN Program:  Revenue & Expenses":"RevExp","Total":"total","YTD Actual":"YTDA",
                             "Budget":"budget", "Budget Adjustments":"budgetA",
                             "Q4 Forecast":"Q4F","Q4 $ Forecast Variance to Budget":"Q4FTB","Q4 % Forecast Variance to Budget":"Q4FTBP",
@@ -23,20 +23,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     # print(df.to_json())
     # db = json.loads(file['body']['$content']) 
     # df = pd.DataFrame(db)
-    if not file:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            file = req_body.get('file')
-
-    if file:   
+  
         # wb = load_workbook(file)  
-        return func.HttpResponse(str(error))
+    return func.HttpResponse(str(error))
         # return func.HttpResponse(wb.get_sheet_names())
-    else:
-        return func.HttpResponse(
-             "Please pass a name on the query string or in the request body",
-             status_code=400
-        )
