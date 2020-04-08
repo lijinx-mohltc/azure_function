349250c12642c6fc908c59aa0255225478c80ab1
Jason Li <jasonli@Jasons-MacBook-Pro.local>
2020-04-08 13:06:12 -0400

    add validation

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -4,16 +4,22 @@ import azure.functions as func
 import base64
 import json
 import pandas as pd
+from .validation import validate
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     file = req.get_json()
-    decrypted=base64.b64decode(file['body']['$content'])
+    decrypted=base64.b64decode(file['$content'])
     toread = io.BytesIO()
     toread.write(decrypted)
     # toread.seek(0)
-    df = pd.read_excel(toread)
+    df = pd.read_excel(toread, usecols="C:K",skiprows=7)
+    df.rename(columns={"LHIN Program:  Revenue & Expenses":"RevExp","Total":"total","YTD Actual":"YTDA",
+                            "Budget":"budget", "Budget Adjustments":"budgetA",
+                            "Q4 Forecast":"Q4F","Q4 $ Forecast Variance to Budget":"Q4FTB","Q4 % Forecast Variance to Budget":"Q4FTBP",
+                            "Comments\nExplanations are required where \nthe Q4 Forecasted % exceeds +/-10%":"comments"},inplace=True)
+    error = validate(df)
     # print(df.to_json())
     # db = json.loads(file['body']['$content']) 
     # df = pd.DataFrame(db)
@@ -27,7 +33,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     if file:   
         # wb = load_workbook(file)  
-        return func.HttpResponse(df.to_json())
+        return func.HttpResponse(str(error))
         # return func.HttpResponse(wb.get_sheet_names())
     else:
         return func.HttpResponse(
---
+++ HttpTrigger1/validation.py
@@ -0,0 +1,47 @@
+import pandas_schema
+from pandas_schema import Column, Schema
+from pandas_schema.validation import CustomElementValidation, LeadingWhitespaceValidation, InListValidation,CustomSeriesValidation
+from pandas_schema.validation import TrailingWhitespaceValidation, CanConvertValidation, MatchesPatternValidation, InRangeValidation 
+    
+from decimal import *
+import numpy as np
+import math
+
+
+
+def check_decimal(dec):
+    try:
+        int(dec)
+    except ValueError:
+        return False
+    return True
+
+def validate(data):
+    decimal_validation = [CustomElementValidation(lambda d: check_decimal(d), 'is not decimal')]
+    null_validation = [CustomElementValidation(lambda d: d , 'this field cannot be null')]
+    test = [CustomElementValidation(lambda d: check_decimal(d), 'invalideted') | CustomElementValidation(lambda d: str(d).__contains__('nan'),"")]
+    range_text = [CustomElementValidation(lambda d: (d>=0)&(d<100), 'not range')]
+    range_number = [CustomElementValidation(lambda d: (d>=0)&(d<10000000), 'not range') | CustomElementValidation(lambda d: str(d).__contains__('nan'),"")]
+   
+    schema = pandas_schema.Schema([
+                Column('RevExp'),
+                Column('budget', test),
+                Column('budgetA', test),
+                Column('total', test),
+                Column('YTDA', test),
+                Column('Q4F', test),
+                Column('Q4FTB',test),
+                Column('Q4FTBP', test),
+                Column('comments')
+                ])
+    errors = schema.validate(data)
+    # for e in errors:
+    #     print(e)
+    # errors_index = {"row":[e.row for e in errors],"column":[e.column for e in errors]}
+    list = []
+    # print(errors)
+    # result = jsonify({"error": tuple(errors)})
+    for e in errors:
+        list.append(str(e))
+    errors_index = {"row":[e.row for e in errors],"column":[e.column for e in errors]}
+    return list
